b29cd27eb815ce9d49a0dadf5cbff9a5412755d5
PwnedSoul39 <nucup2130688@mls.ceu.edu.ph>
2025-11-27 22:44:10 +0800

    added components 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,12 @@ import "~/styles/globals.css";
 
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
-import { HeroSection1 } from "~/components/pro-blocks/landing-page/hero-sections/hero-section-1";
+import { PricingSection1 } from "~/components/pro-blocks/landing-page/pricing-sections/pricing-section-1";
 import { BlogSection1 } from "~/components/pro-blocks/landing-page/blog-sections/blog-section-1";
 import { LpNavbar1 } from "~/components/pro-blocks/landing-page/lp-navbars/lp-navbar-1";
 import { Footer1 } from "~/components/pro-blocks/landing-page/footers/footer-1";
+import { ContactSection1 } from "~/components/pro-blocks/landing-page/contact-sections/contact-section-1";
+import { LogoSection1 } from "~/components/pro-blocks/landing-page/logo-sections/logo-section-1";
 
 export const metadata: Metadata = {
   title: "Test Site",
@@ -25,8 +27,13 @@ export default function RootLayout({
     <html lang="en" className={`${geist.variable}`}>
       <body>
         <LpNavbar1 />
+        <LogoSection1 />
+        <hr />
         <BlogSection1 />
-        <HeroSection1 />
+        <hr />
+        <PricingSection1 />
+        <hr />
+        <ContactSection1 />
         <Footer1 />
       </body>
     </html>
