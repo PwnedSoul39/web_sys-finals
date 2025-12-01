b61c3d82f93080e85b2443c1dbb3acfdb42d3e8e
PwnedSoul39 <nucup2130688@mls.ceu.edu.ph>
2025-12-01 17:16:12 +0800

    yet another fix 4

--- src/components/pro-blocks/landing-page/lp-navbars/lp-navbar-1.tsx
+++ src/components/pro-blocks/landing-page/lp-navbars/lp-navbar-1.tsx
@@ -7,10 +7,10 @@ import { useState } from "react";
 import Link from "next/link";
 
 const MENU_ITEMS = [
-  { label: "Home", href: "#home" },
-  { label: "News", href: "#news" },
-  { label: "Pricing", href: "#pricing" },
-  { label: "Contact", href: "#contact" },
+  { label: "Home", href: "/app#home" },
+  { label: "News", href: "/app#news" },
+  { label: "Pricing", href: "/app#pricing" },
+  { label: "Contact", href: "/app#contact" },
 ] as const;
 
 interface NavMenuItemsProps {
@@ -19,8 +19,8 @@ interface NavMenuItemsProps {
 
 const NavMenuItems = ({ className }: NavMenuItemsProps) => (
   <div className={`flex flex-col gap-1 md:flex-row ${className ?? ""}`}>
-    {MENU_ITEMS.map(({ label, href }) => (
-      <Link key={label} href={href} scroll={false} passHref>
+    {MENU_ITEMS.map(({ label, href, link }) => (
+      <Link key={label} href={href} scroll={true}>
         <Button variant="ghost" className="w-full md:w-auto">
           {label}
         </Button>
@@ -38,7 +38,7 @@ export function LpNavbar1() {
     <nav className="bg-gray-950 sticky top-0 isolate z-50 py-3.5 md:py-4 text-white">
       <div className="relative container m-auto flex flex-col justify-between gap-4 px-6 md:flex-row md:items-center md:gap-6">
         <div className="flex items-center justify-between">
-          <Link href="#" aria-label="Go to homepage">
+          <Link href="/app" aria-label="Go to homepage">
             <Logo className="size-7" />
           </Link>
           <Button
